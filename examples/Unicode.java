0749859a6202287a694230ccac7b806a0043715d
ekincanufuktepe <ekincanufuktepe@gmail.com>
2021-04-13 23:52:26 -0500

    Initial commit
    Basic Maven and Antlr setup are made for the environment.

---
+++ examples/Unicode.java
@@ -0,0 +1,10 @@
+class Unicode {
+        public static void main(String[] args) {
+                System.out.println("A = \uuu0041");
+        }
+        private void printSomething() {
+        	int a = 6;
+        	double x = 2.1;
+        	System.out.println(a+x);
+        }
+}
---
+++ src/main/java/cij/Main.java
@@ -0,0 +1,10 @@
+package cij;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
