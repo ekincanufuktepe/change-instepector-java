7a06a8e36007ab9b32404644194a880cca5e992d
ekincanufuktepe <ekincanufuktepe@gmail.com>
2021-05-18 12:44:19 -0500

    Class data collection for modifiers and name added
    Currently only class name information and modifiers of a class are
    extracted. However, the parent class information are not includede yet.

--- src/main/java/cij/changerules/ClassInformationDataCollector.java
+++ src/main/java/cij/changerules/ClassInformationDataCollector.java
@@ -1,5 +1,63 @@
 package cij.changerules;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import cij.changerules.classinfo.ClassInformation;
+import cij.grammar.java.CodeComponentNode;
+
 public class ClassInformationDataCollector {
+	
+	private Set<ClassInformation> classList = new HashSet<>();
+
+	public Set<ClassInformation> getClassList() {
+		return classList;
+	}
+
+	public void setMethodList(Set<ClassInformation> classList) {
+		this.classList = classList;
+	}
+
+	public void collectClasses(CodeComponentNode root) {
+		if(root.getType().equals("typeDeclaration")) {
+			ClassInformation ci = new ClassInformation();
+			CodeComponentNode baseClass = getBaseClass(root);
+			/*
+			 * TODO: First index seems to be the type of the class, 
+			 * class, interface etc. Test what would happen with an interface.
+			 * In a class the type would be simply "class".
+			 */
+			
+			// Initialize base class name
+			// Second index is where the class name is
+			ci.setClassName(baseClass.getCodeList().get(1));
+			// initialize base class modifiers
+			ci.setModifiers(getBaseClassModifiers(baseClass));
+			classList.add(ci);
+		}
+
+	}
+
+	private CodeComponentNode getBaseClass(CodeComponentNode node) {
+		for(CodeComponentNode child : node.getChildren()) {
+			if(child.getType().equals("(classDeclaration")) {
+				// get first child of class declaration
+				return child.getChildren().get(0);
+			}
+		}
+		return null;
+	}
+	
+	private Set<String> getBaseClassModifiers(CodeComponentNode node) {
+		Set<String> modifiers = new HashSet<>();
+		for(CodeComponentNode child : node.getChildren()) {
+			if(child.getType().equals("(classModifier")) {
+				for(String modifier : child.getCodeList()) {
+					modifiers.add(modifier);
+				}
+			}
+		}
+		return modifiers;
+	}
 
 }
--- src/main/java/cij/changerules/classinfo/ClassInformation.java
+++ src/main/java/cij/changerules/classinfo/ClassInformation.java
@@ -1,5 +1,85 @@
 package cij.changerules.classinfo;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ClassInformation {
 
+	private String className;
+	private Set<String> parentClassNames = new HashSet<>();
+	private Set<String> modifiers = new HashSet<>();
+	
+	public ClassInformation() {
+
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public Set<String> getParentClassNames() {
+		return parentClassNames;
+	}
+
+	public void setParentClassNames(Set<String> parentClassNames) {
+		this.parentClassNames = parentClassNames;
+	}
+
+	public Set<String> getModifiers() {
+		return modifiers;
+	}
+
+	public void setModifiers(Set<String> modifiers) {
+		this.modifiers = modifiers;
+	}
+
+	@Override
+	public String toString() {
+		return "ClassInformation [className=" + className + ", parentClassNames=" + parentClassNames + ", modifiers="
+				+ modifiers + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((className == null) ? 0 : className.hashCode());
+		result = prime * result + ((modifiers == null) ? 0 : modifiers.hashCode());
+		result = prime * result + ((parentClassNames == null) ? 0 : parentClassNames.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClassInformation other = (ClassInformation) obj;
+		if (className == null) {
+			if (other.className != null)
+				return false;
+		} else if (!className.equals(other.className))
+			return false;
+		if (modifiers == null) {
+			if (other.modifiers != null)
+				return false;
+		} else if (!modifiers.equals(other.modifiers))
+			return false;
+		if (parentClassNames == null) {
+			if (other.parentClassNames != null)
+				return false;
+		} else if (!parentClassNames.equals(other.parentClassNames))
+			return false;
+		return true;
+	}
+	
+	
+	
 }
