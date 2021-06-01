6d7cfddcae122c988474da080d6d78ed7d3782e9
ekincanufuktepe <ekincanufuktepe@gmail.com>
2021-06-01 15:52:51 -0500

    Field Data collection class and ADT added
    Class for collecting field information (field name, filed modifiers)
    added. Also the Field abstract data type class is created.

--- src/main/java/cij/changerules/FieldInformationDataCollector.java
+++ src/main/java/cij/changerules/FieldInformationDataCollector.java
@@ -1,5 +1,68 @@
 package cij.changerules;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import cij.changerules.field.FieldInformation;
+import cij.grammar.java.CodeComponentNode;
+
 public class FieldInformationDataCollector {
+
+	private Set<FieldInformation> fieldList = new HashSet<>();
+
+	public Set<FieldInformation> getFieldList() {
+		return fieldList;
+	}
+
+	public void setClassList(Set<FieldInformation> fieldList) {
+		this.fieldList = fieldList;
+	}
+
+	public void collectFields(CodeComponentNode root) {
+		// Find and get the node where the field is declared
+		if(root.getType().equals("(fieldDeclaration")) {
+			FieldInformation fi = new FieldInformation();
+
+			// get and set the field name
+			fi.setFieldName(getFieldName(root));
+			// get and set the field modifiers
+			fi.setFieldModifiers(getFieldModifiers(root));
+			fieldList.add(fi);
+		}
+		else {
+			for(CodeComponentNode child : root.getChildren()) {
+				collectFields(child);
+			}
+		}
+		return;
+	}
+
+
+	private Set<String> getFieldModifiers(CodeComponentNode root) {
+		Set<String> modifiers = new HashSet<>();
+		for(CodeComponentNode child : root.getChildren()) {
+			if(child.getType().equals("(fieldModifier")) {
+				for(String modifier : child.getCodeList()) {
+					modifier = modifier.replace("(", "");
+					modifier = modifier.replace(")", "");
+					modifiers.add(modifier);
+				}
+			}
+		}
+		return modifiers;
+	}
+
+	private String getFieldName(CodeComponentNode root) {
+		String fieldName = "";
+		if(root.getType().equals("(variableDeclaratorId")) {
+			fieldName = root.getCodeList().get(0).replace("(", "");
+			fieldName = fieldName.replace(")", "");
+			return fieldName;
+		}
+			for(CodeComponentNode child : root.getChildren()) {
+				fieldName = fieldName + getFieldName(child);
+			}
+		return fieldName;
+	}
 
 }
--- src/main/java/cij/changerules/field/FieldInformation.java
+++ src/main/java/cij/changerules/field/FieldInformation.java
@@ -1,5 +1,36 @@
 package cij.changerules.field;
 
-public class FieldInformation {
+import java.util.HashSet;
+import java.util.Set;
 
+public class FieldInformation {
+	private String fieldName;
+	private Set<String> fieldModifiers = new HashSet<>();
+	private String fieldType;
+	
+	public FieldInformation() {
+		
+	}
+	
+	public String getFieldName() {
+		return fieldName;
+	}
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+	public Set<String> getFieldModifiers() {
+		return fieldModifiers;
+	}
+	public void setFieldModifiers(Set<String> fieldModifiers) {
+		this.fieldModifiers = fieldModifiers;
+	}
+	public String getFieldType() {
+		return fieldType;
+	}
+	public void setFieldType(String fieldType) {
+		this.fieldType = fieldType;
+	}
+	
+	
+	
 }
