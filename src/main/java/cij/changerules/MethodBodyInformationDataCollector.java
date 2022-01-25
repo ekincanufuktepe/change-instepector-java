68d500a993663d5d4e41a028280354fcdbe78ac0
ekincanufuktepe <ekincanufuktepe@gmail.com>
2022-01-24 23:16:43 -0600

    Data collection for For-statements included
    For-statement information collector methods are added. Also the
    if-statement collection entry method is generalized by not limiting for
    if-statement collection, but also for for-statements.
    TODO: The collection for-statement types is not that elegant, can be
    designed in a better way. Could just use one single method for that.

--- src/main/java/cij/changerules/MethodBodyInformationDataCollector.java
+++ src/main/java/cij/changerules/MethodBodyInformationDataCollector.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 import cij.changerules.method.body.info.Expression;
+import cij.changerules.method.body.info.ForStatement;
 import cij.changerules.method.body.info.IfStatement;
 import cij.changerules.method.body.info.MethodInvocation;
 import cij.changerules.method.body.info.Statement;
@@ -14,19 +15,31 @@ import cij.grammar.java.CodeComponentNode;
 public class MethodBodyInformationDataCollector {
 
 	private List<IfStatement> ifStmts = new ArrayList<IfStatement>();
+	private List<ForStatement> forStmts = new ArrayList<ForStatement>();
 	private Set<MethodInvocation> methodInvocationSet = new HashSet<MethodInvocation>(); 
 
-	public void collectMethodIfStatement(CodeComponentNode root) {
+	public void collectMethodBodyDetails(CodeComponentNode root) {
 		if((root.getType().equals("(ifThenStatement") || root.getType().equals("(ifThenElseStatement")) 
 				&& !root.getCodeList().isEmpty()) {
 			IfStatement ifStmt = new IfStatement();
 			ifStmt = initializeIfStatment(root, ifStmt);
 			// add to if-statements list
+//			System.out.println("IF STATEMENT: " + ifStmt);
 			ifStmts.add(ifStmt);
+		}
+		else if(root.getType().equals("(forStatement")) {
+			ForStatement forStmt = new ForStatement();
+			forStmt = initializeForStatment(root, forStmt);
+			for(CodeComponentNode child : root.getChildren()) {
+				forStmt = initializeForStatment(child, forStmt);
+				// add to if-statements list
+			}
+			// add to for-statements list
+			forStmts.add(forStmt);
 		}
 		else {
 			for(CodeComponentNode child : root.getChildren()) {
-				collectMethodIfStatement(child);
+				collectMethodBodyDetails(child);
 			}
 		}
 	}
@@ -48,7 +61,7 @@ public class MethodBodyInformationDataCollector {
 //				collectBlockStatementsInfo(child, ifStmt);
 				// search for chained if-then-else statements
 				for(CodeComponentNode chainedIf : child.getChildren()) {
-					collectMethodIfStatement(chainedIf);
+					collectMethodBodyDetails(chainedIf);
 				}
 			}
 		}
@@ -69,27 +82,145 @@ public class MethodBodyInformationDataCollector {
 		}
 	}
 
-	/*
-	 * TODO: This where the chained if statements are added as a part of the upper if statement! 
-	 * */
-	// collects the conditions written for the if statement
-	public void collectExpressionInfo(CodeComponentNode ifNode, IfStatement ifStmt) {
+
+	public void collectExpressionInfo(CodeComponentNode ifNode, Object stmt) {
 		for(CodeComponentNode child : ifNode.getChildren()) {
 			if(!child.getCodeList().isEmpty()) {
 				if(child.getType().equals("(ifThenStatement") || child.getType().equals("(ifThenElseStatement")) {
-//					System.out.println("ISSUE HERE!");
 					IfStatement chainedIf = new IfStatement();
 					chainedIf = initializeIfStatment(child, chainedIf);
-					ifStmt.getIfStatements().add(chainedIf);
+					((IfStatement)stmt).getIfStatements().add(chainedIf);
 					continue;
 				}
 				else {
 					Expression exp = new Expression(child.getType(), child.getCodeList());
-					ifStmt.getExpressions().add(exp);
+					if(stmt.getClass().isInstance(new IfStatement())) {
+						((IfStatement)stmt).getExpressions().add(exp);
+					}	
 //					collectExpressionInfo(child, ifStmt);
 				}
 			}
-			collectExpressionInfo(child, ifStmt);
+			collectExpressionInfo(child, stmt);
+		}
+	}
+	
+	public void collectExpressionForForInit(CodeComponentNode ifNode, Object stmt) {
+		for(CodeComponentNode child : ifNode.getChildren()) {
+			if(!child.getCodeList().isEmpty()) {
+				if(child.getType().equals("(ifThenStatement") || child.getType().equals("(ifThenElseStatement")) {
+					IfStatement chainedIf = new IfStatement();
+					chainedIf = initializeIfStatment(child, chainedIf);
+					((IfStatement)stmt).getIfStatements().add(chainedIf);
+					continue;
+				}
+				else {
+					Expression exp = new Expression(child.getType(), child.getCodeList());
+					if(stmt.getClass().isInstance(new IfStatement())) {
+						((IfStatement)stmt).getExpressions().add(exp);
+					}
+					else if(stmt.getClass().isInstance(new ForStatement())){
+//						System.out.println("Detected ForStatement");
+						((ForStatement)stmt).getForInits().add(exp);
+					}
+				}
+			}
+			collectExpressionForForInit(child, stmt);
+		}
+	}
+	
+	public void collectExpressionForForExpression(CodeComponentNode ifNode, Object stmt) {
+		for(CodeComponentNode child : ifNode.getChildren()) {
+			if(!child.getCodeList().isEmpty()) {
+				if(child.getType().equals("(ifThenStatement") || child.getType().equals("(ifThenElseStatement")) {
+					IfStatement chainedIf = new IfStatement();
+					chainedIf = initializeIfStatment(child, chainedIf);
+					((IfStatement)stmt).getIfStatements().add(chainedIf);
+					continue;
+				}
+				else {
+					Expression exp = new Expression(child.getType(), child.getCodeList());
+					if(stmt.getClass().isInstance(new IfStatement())) {
+						((IfStatement)stmt).getExpressions().add(exp);
+					}
+					else if(stmt.getClass().isInstance(new ForStatement())){
+//						System.out.println("Detected ForStatement");
+						((ForStatement)stmt).getForExpressions().add(exp);
+					}
+				}
+			}
+			collectExpressionForForExpression(child, stmt);
+		}
+	}
+	
+	public void collectExpressionForForUpdate(CodeComponentNode ifNode, Object stmt) {
+		for(CodeComponentNode child : ifNode.getChildren()) {
+			if(!child.getCodeList().isEmpty()) {
+				if(child.getType().equals("(ifThenStatement") || child.getType().equals("(ifThenElseStatement")) {
+					IfStatement chainedIf = new IfStatement();
+					chainedIf = initializeIfStatment(child, chainedIf);
+					((IfStatement)stmt).getIfStatements().add(chainedIf);
+					continue;
+				}
+				else {
+					Expression exp = new Expression(child.getType(), child.getCodeList());
+					if(stmt.getClass().isInstance(new IfStatement())) {
+						((IfStatement)stmt).getExpressions().add(exp);
+					}
+					else if(stmt.getClass().isInstance(new ForStatement())){
+//						System.out.println("Detected ForStatement");
+						((ForStatement)stmt).getForUpdates().add(exp);
+					}
+				}
+			}
+			collectExpressionForForUpdate(child, stmt);
+		}
+	}
+	
+	public void collectExpressionForForVariableId(CodeComponentNode ifNode, Object stmt) {
+		for(CodeComponentNode child : ifNode.getChildren()) {
+			if(!child.getCodeList().isEmpty()) {
+				if(child.getType().equals("(ifThenStatement") || child.getType().equals("(ifThenElseStatement")) {
+					IfStatement chainedIf = new IfStatement();
+					chainedIf = initializeIfStatment(child, chainedIf);
+					((IfStatement)stmt).getIfStatements().add(chainedIf);
+					continue;
+				}
+				else {
+					Expression exp = new Expression(child.getType(), child.getCodeList());
+					if(stmt.getClass().isInstance(new IfStatement())) {
+						((IfStatement)stmt).getExpressions().add(exp);
+					}
+					else if(stmt.getClass().isInstance(new ForStatement())){
+//						System.out.println("Detected ForStatement");
+						((ForStatement)stmt).getForUpdates().add(exp);
+					}
+				}
+			}
+			collectExpressionForForVariableId(child, stmt);
+		}
+	}
+	
+	public void collectExpressionForForUannType(CodeComponentNode ifNode, Object stmt) {
+		for(CodeComponentNode child : ifNode.getChildren()) {
+			if(!child.getCodeList().isEmpty()) {
+				if(child.getType().equals("(ifThenStatement") || child.getType().equals("(ifThenElseStatement")) {
+					IfStatement chainedIf = new IfStatement();
+					chainedIf = initializeIfStatment(child, chainedIf);
+					((IfStatement)stmt).getIfStatements().add(chainedIf);
+					continue;
+				}
+				else {
+					Expression exp = new Expression(child.getType(), child.getCodeList());
+					if(stmt.getClass().isInstance(new IfStatement())) {
+						((IfStatement)stmt).getExpressions().add(exp);
+					}
+					else if(stmt.getClass().isInstance(new ForStatement())){
+//						System.out.println("Detected ForStatement");
+						((ForStatement)stmt).getForUnannType().add(exp);
+					}
+				}
+			}
+			collectExpressionForForUannType(child, stmt);
 		}
 	}
 
@@ -141,6 +272,70 @@ public class MethodBodyInformationDataCollector {
 			collectMethodInvocations(child, methodInvocation);
 		}
 	}
+	
+	public void collectMethodForStatement(CodeComponentNode root) {
+		if(root.getType().equals("(forStatement") && !root.getCodeList().isEmpty()) {
+			ForStatement forStmt = new ForStatement();
+			for(CodeComponentNode child : root.getChildren()) {
+				forStmt = initializeForStatment(child, forStmt);
+				// add to if-statements list
+			}
+			forStmts.add(forStmt);
+		}
+		else {
+			for(CodeComponentNode child : root.getChildren()) {
+				collectMethodForStatement(child);
+			}
+		}
+	}
+	
+	public ForStatement initializeForStatment(CodeComponentNode root, ForStatement forStmt) {
+		for(CodeComponentNode child : root.getChildren()) {
+//			System.out.println("CHILD: "+ child.getType());
+			// for-statement expression/condition
+			if(child.getType().equals("(expression")) {
+				// expression information collected and added to the given if-statement
+//				System.out.println("FOREXP");
+				collectExpressionForForExpression(child, forStmt);
+			}
+			// for-statement initialization
+			else if(child.getType().equals("(forInit")) {
+//				System.out.println("FORINIT");
+				collectExpressionForForInit(child, forStmt);
+			}
+			// for-statement update
+			else if(child.getType().equals("(forUpdate")) {
+//				collectBlockStatementsInfo(child, ifStmt);
+				// search for chained if-then-else statements
+//				System.out.println("FORUPDATE");
+				for(CodeComponentNode chainedFor : child.getChildren()) {
+					collectExpressionForForUpdate(chainedFor, forStmt);
+				}
+			}
+			else if(child.getType().equals("(unannType")) {
+//				System.out.println("FORUANNTYPE");
+				for(CodeComponentNode chainedFor : child.getChildren()) {
+					collectExpressionForForUannType(chainedFor, forStmt);
+				}
+			}
+			else if(child.getType().equals("(variableDeclaratorId")) {
+//				System.out.println("FORVARIABLE");
+				for(CodeComponentNode chainedFor : child.getChildren()) {
+					collectExpressionForForVariableId(chainedFor, forStmt);
+				}
+			}
+			// check for inner 
+			else if(child.getType().equals("(statement")) {
+//				collectBlockStatementsInfo(child, ifStmt);
+				// search for chained if-then-else statements
+				for(CodeComponentNode chainedFor : child.getChildren()) {
+					collectMethodBodyDetails(chainedFor);
+				}
+			}
+		}
+		return forStmt;
+	}
+	
 
 	public List<IfStatement> getIfStmts() {
 		return ifStmts;
@@ -156,5 +351,13 @@ public class MethodBodyInformationDataCollector {
 
 	public void setMethodInvocationSet(Set<MethodInvocation> methodInvocationSet) {
 		this.methodInvocationSet = methodInvocationSet;
+	}
+
+	public List<ForStatement> getForStmts() {
+		return forStmts;
+	}
+
+	public void setForStmts(List<ForStatement> forStmts) {
+		this.forStmts = forStmts;
 	}
 }
